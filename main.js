57302a74cead6991e1194050e5262271c1928087
handan <lvlvchunyan@126.com>
2019-06-13 10:37:22 +0800

    '测试上传'

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const { app, BrowserWindow } = require('electron')
 
 // 保持对window对象的全局引用，如果不这么做的话，当JavaScript对象被
 // 垃圾回收的时候，window对象将会自动的关闭
-let win
+let win;
 
 function createWindow () {
     // 创建浏览器窗口。
@@ -12,27 +12,27 @@ function createWindow () {
         webPreferences: {
             nodeIntegration: true
         }
-    })
+    });
 
     // 加载index.html文件
-    win.loadFile('index.html')
+    win.loadFile('index.html');
 
     // 打开开发者工具
-    win.webContents.openDevTools()
+    win.webContents.openDevTools();
 
     // 当 window 被关闭，这个事件会被触发。
     win.on('closed', () => {
         // 取消引用 window 对象，如果你的应用支持多窗口的话，
         // 通常会把多个 window 对象存放在一个数组里面，
         // 与此同时，你应该删除相应的元素。
-        win = null
-    })
+        win = null;
+    });
 }
 
 // Electron 会在初始化后并准备
 // 创建浏览器窗口时，调用这个函数。
 // 部分 API 在 ready 事件触发后才能使用。
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // 当全部窗口关闭时退出。
 app.on('window-all-closed', () => {
@@ -49,7 +49,7 @@ app.on('activate', () => {
     if (win === null) {
         createWindow()
     }
-})
+});
 
 // 在这个文件中，你可以续写应用剩下主进程代码。
 // 也可以拆分成几个文件，然后用 require 导入。
